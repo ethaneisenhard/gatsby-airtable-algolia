83741c94407bfccaf8a6a3a0f52a9775a1e0a0d4
Ethan Eisenhard <eeisenhard@tollbrothers.com>
2019-07-18 12:04:58 -0400

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,20 +42,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/src/assets/`
-      }
-    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
